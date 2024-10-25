60d27d1fccc3800a0dc4ed2d76c03e9911e9449e
TrungHieu <nguyentrunghieu250303@gmail.com>
2024-10-25 16:59:02 +0700

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.print("Hello");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
